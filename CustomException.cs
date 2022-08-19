ac664c8425d4116d3ea980925a3c4c26bb97ec26
mandara11 <mandaram2345@gmail.com>
2022-08-19 20:50:08 +0530

    InformUserifenteredInvalidMood

---
+++ CustomException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem  //UC3
+{
+    public class CustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_MESSAGE, EMPTY_MESSAGE,
+            CONSTRUCTOR_NOT_FOUND,
+            CLASS_NOT_FOUND
+        }
+        public ExceptionType Type;
+
+        public CustomException(ExceptionType type, string message) : base(message)
+        {
+            Type = type;
+        }
+    }
+}
