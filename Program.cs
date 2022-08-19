aeeedfdac9e51716e638a9479f7d2b5e9b236f84
mandara11 <mandaram2345@gmail.com>
2022-08-19 19:27:04 +0530

    Added:UC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to MoodAnalyzer Problem");
+
+            HappySad happySad = new HappySad("i am in sad a mood");
+            Console.WriteLine(happySad.AnalysingMood());
         }
     }
 }
